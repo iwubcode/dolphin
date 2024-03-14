c2396047b809b0147858370d5d90ac82a1599e19
iwubcode <iwubcode@users.noreply.github.com>
2024-03-14 08:31:32 -0500

    active target panel

--- Source/Core/VideoCommon/GraphicsModEditor/Panels/ActiveTargetsPanel.h
+++ Source/Core/VideoCommon/GraphicsModEditor/Panels/ActiveTargetsPanel.h
@@ -22,38 +22,18 @@ public:
   // Renders ImGui windows to the currently-bound framebuffer.
   void DrawImGui();
 
-  void AddDrawCall(DrawCallData draw_call);
-  void AddFBCall(FBCallData fb_call);
-  void AddLightData(LightData light_data);
-
 private:
-  void DrawCallPanel();
-  void EFBPanel();
-  void LightPanel();
-  void EndOfFrame();
-  void SelectionChanged();
-  Common::EventHook m_end_of_frame_event;
+  void DrawCallPanel(const std::vector<GraphicsModEditor::RuntimeState::XFBData*>& xfbs);
+  void EFBPanel(const std::vector<GraphicsModEditor::RuntimeState::XFBData*>& xfbs);
+  void LightPanel(const std::vector<GraphicsModEditor::RuntimeState::XFBData*>& xfbs);
   Common::EventHook m_selection_event;
 
   EditorState& m_state;
 
-  // Target tracking
-  std::vector<const DrawCallData*> m_current_draw_calls;
-  std::vector<const DrawCallData*> m_draw_calls_to_clean;
-  std::map<GraphicsMods::DrawCallID, DrawCallData> m_new_draw_call_id_to_data;
-
-  std::vector<FBCallData*> m_current_fb_calls;
-  std::vector<FBCallData*> m_upcoming_fb_calls;
-  std::map<FBInfo, FBCallData> m_upcoming_fb_call_id_to_data;
-
-  std::vector<const LightData*> m_current_lights;
-  std::vector<const LightData*> m_lights_to_clean;
-  std::map<GraphicsMods::LightID, LightData> m_new_light_id_to_data;
-
   // Track open nodes
-  std::set<GraphicsMods::DrawCallID> m_open_draw_call_nodes;
-  std::set<FBInfo> m_open_fb_call_nodes;
-  std::set<GraphicsMods::LightID> m_open_light_nodes;
+  std::set<GraphicsModSystem::DrawCallID> m_open_draw_call_nodes;
+  std::set<GraphicsModSystem::TextureCacheID, std::less<>> m_open_texture_call_nodes;
+  std::set<GraphicsModSystem::LightID> m_open_light_nodes;
 
   // Selected nodes
   std::set<SelectableType> m_selected_nodes;
